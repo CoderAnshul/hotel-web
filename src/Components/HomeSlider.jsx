6d8677508c1f1d4c20e7bf7bfedcf4e34fe36b7e
Anshul Sharma <anshul.sharma6163@gmail.com>
2024-07-27 01:36:08 +0530

    code updation

--- src/Components/HomeSlider.jsx
+++ src/Components/HomeSlider.jsx
@@ -1,19 +1,13 @@
 import React, { useState, useEffect } from "react";
-
-const images = [
-  "src/assets/Images/slide1.jpg",
-  "src/assets/Images/slide2.jpg",
-  "src/assets/Images/slide3.jpg",
-];
-
+import img1 from "../assets/Images/slide1.jpg";
+import img2 from "../assets/Images/slide2.jpg";
+import img3 from "../assets/Images/slide3.jpg";
 import svg1 from "../assets/Images/location.png";
 import svg2 from "../assets/Images/calender.png";
 import svg3 from "../assets/Images/room.png";
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
-
-
-
+const images = [img1, img2, img3];
 
 const HomeSlider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -23,9 +17,7 @@ const HomeSlider = () => {
   };
 
   const prevSlide = () => {
-    setCurrentIndex(
-      (prevIndex) => (prevIndex - 1 + images.length) % images.length
-    );
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
   };
 
   const setSlide = (index) => {
@@ -41,9 +33,11 @@ const HomeSlider = () => {
   }, []);
 
   return (
-    <div className="wrapper relative flex flex-col items-center ">
+    <div className="wrapper relative flex flex-col items-center">
       <div className="relative flex justify-center w-full !lg:w-[100%] h-[60vh] sm:h-[70vh] md:h-[80vh] overflow-hidden">
-      <div className="heading absolute z-50 text-[42px] sm:text-[58px] md:text-[66px] lg:text-[78px] w-[65%] leading-10 sm:leading-[50px] md:leading-[60px] font-fatFace text-white -mt-16 text-center top-1/2 left-1/2 translate-x-[-50%]">More than Just a Stay</div>
+        <div className="heading absolute z-50 text-[42px] sm:text-[58px] md:text-[66px] lg:text-[78px] w-[65%] leading-10 sm:leading-[50px] md:leading-[60px] font-fatFace text-white -mt-16 text-center top-1/2 left-1/2 translate-x-[-50%]">
+          More than Just a Stay
+        </div>
         <div className="darkoverlay absolute h-full w-full bg-[#00000069] z-20"></div>
 
         <div
@@ -76,19 +70,16 @@ const HomeSlider = () => {
           {images.map((_, index) => (
             <span
               key={index}
-              className={`h-3 w-3  rounded-full cursor-pointer ${
-                index === currentIndex ? "bg-white" : "bg-black bg-opacity-50"
-              }`}
+              className={`h-3 w-3 rounded-full cursor-pointer ${index === currentIndex ? "bg-white" : "bg-black bg-opacity-50"}`}
               onClick={() => setSlide(index)}
             ></span>
           ))}
         </div>
       </div>
 
-      {/* ............................... Search field ............................... */}
-
+      {/* Search Field */}
       <div className="searchBox h-32 w-[84%] pl-6 bg-red-50 absolute z-50 -bottom-16 rounded-2xl shadow-xl gap-5 items-center p-2 hidden lg:flex">
-        <div className="search h-36 w-[20%]  pt-6">
+        <div className="search h-36 w-[20%] pt-6">
           <h5 className="mb-4 font-primaryMedium text-xs">Destination</h5>
           <div className="input flex relative h-12 w-[90%] bg-white rounded-lg p-4 font-primaryMedium items-center overflow-hidden">
             <input
@@ -96,7 +87,7 @@ const HomeSlider = () => {
               placeholder="Choose City"
               className="ml-5 p-4 outline-none text-sm w-[90%]"
             />
-            <img className="h-20 absolute  -ml-8" src={svg1} alt="" />
+            <img className="h-20 absolute -ml-8" src={svg1} alt="" />
           </div>
         </div>
         <div className="search h-36 w-[20%] pt-6">
@@ -106,17 +97,17 @@ const HomeSlider = () => {
             <img className="h-20 absolute -ml-8" src={svg2} alt="" />
           </div>
         </div>
-        <div className="search h-36 w-[20%]  pt-6">
+        <div className="search h-36 w-[20%] pt-6">
           <h5 className="mb-4 font-primaryMedium text-xs">Check-out</h5>
           <div className="input flex items-center relative h-12 w-[90%] bg-white rounded-lg p-4 font-primaryMedium">
-            <input type="date" className="ml-5 pl-2 text-sm opacity-65 outline-none w-[90%] "  />
-            <img className="h-20 absolute  -ml-8" src={svg2} alt="" />
+            <input type="date" className="ml-5 pl-2 text-sm opacity-65 outline-none w-[90%]" />
+            <img className="h-20 absolute -ml-8" src={svg2} alt="" />
           </div>
         </div>
-        <div className="search h-36 w-[20%]  pt-6">
+        <div className="search h-36 w-[20%] pt-6">
           <h5 className="mb-4 font-primaryMedium text-xs">Rooms</h5>
           <div className="input flex items-center text-sm opacity-65 relative h-12 w-[90%] bg-white rounded-lg p-4 font-primaryMedium">
-            <img className="h-20 w-24  -ml-8" src={svg3} alt="" /> Rooms
+            <img className="h-20 w-24 -ml-8" src={svg3} alt="" /> Rooms
           </div>
         </div>
         <button className="search h-12 mt-8 w-[12%] bg-[#702B58] rounded-3xl text-white">
@@ -124,10 +115,10 @@ const HomeSlider = () => {
         </button>
       </div>
 
-
-      {/* ............... search for small devices .................. */}
+      {/* Search for small devices */}
       <div className="search h-20 w-[94%] absolute z-50 -bottom-[90px] lg:hidden">
-        <input placeholder="Search For our destination" type="text" className="w-full shadow-xl pl-20 text-xl h-16 rounded-full"/><SearchOutlinedIcon className="absolute scale-125 opacity-75 z-50 left-0 mt-6 ml-8"/>
+        <input placeholder="Search For our destination" type="text" className="w-full shadow-xl pl-20 text-xl h-16 rounded-full" />
+        <SearchOutlinedIcon className="absolute scale-125 opacity-75 z-50 left-0 mt-6 ml-8" />
       </div>
     </div>
   );
